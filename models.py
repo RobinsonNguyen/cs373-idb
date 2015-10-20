7ef6167b65b0d783e6550ba1ca5cc53bdf6c0b10
Brett Kercher <brettkercher@utexas.edu>
2015-10-20 16:32:13 -0500

    Load pokemon data from json

--- models.py
+++ models.py
@@ -1,22 +1,34 @@
+import json
 
 class PokemonContainer:
 
+	class Move:
+		def __init__(self):
+			self.name = ""
+			self.learn_type = ""
+			self.level = ""
+			self.tm = ""
+
+	class Evolution:
+		def __init__(self):
+			self.to = ""
+			self.method = ""
+			self.level = 0
+
 	class Pokemon:
 
-		def __init__ (self, _id, _name, _type, _egg, _stats, _img):
-			self.id = _id
-			self.name = _name
-			self.type = _type
-			self.eggGroup = _egg
-			self.stats = _stats
-			self.imgPath = _img
+		def __init__ (self):
+			self.id = 0
+			self.name = ""
+			self.type = ()
+			self.eggGroup = ()
+			self.stats = {}
+			self.imgPath = ""
+			self.moves = []
+			self.evolution = []
 
 	def __init__ (self):
-		x = []
-		x.append(self.Pokemon(1, 'Bulbasaur', ('Grass', 'Poison'), ('Monster', 'Grass'), { 'HP': 45, 'ATK': 49, 'DEF': 49, 'SPA': 65, 'SPD': 65, 'SPE': 45 }, '001Bulbasaur'))
-		x.append(self.Pokemon(2, 'Ivysaur', ('Grass', 'Poison'), ('Monster', 'Grass'), { 'HP': 60, 'ATK': 62, 'DEF': 63, 'SPA': 80, 'SPD': 80, 'SPE': 60 }, '002Ivysaur'))
-		x.append(self.Pokemon(3, 'Venusaur', ('Grass', 'Poison'), ('Monster', 'Grass'), { 'HP': 80, 'ATK': 100, 'DEF': 123, 'SPA': 122, 'SPD': 12, 'SPE': 80 }, '003Venusaur'))
-		self.pokemon = x
+		self.ReadPokemonData()
 
 	def GetPokemonById(self, id):
 		for p in self.pokemon:
@@ -25,3 +37,70 @@ class PokemonContainer:
 
 	def GetAllPokemon(self):
 		return self.pokemon
+
+	def ReadPokemonData(self):
+		x = []
+
+		pokemonData = open('./static/json/pokemon_data.txt').read()
+
+		data = json.loads(pokemonData)
+		pokeList = data['pokemon']
+
+		for poke in pokeList:
+			p = self.Pokemon()
+			for key, value in poke.items():
+				if key == 'pkdx_id':
+					p.id = value
+				if key == 'name':
+					p.name = value
+				if key == 'hp':
+					p.stats['HP'] = value
+				if key == 'attack':
+					p.stats['ATK'] = value
+				if key == 'defense':
+					p.stats['DEF'] = value
+				if key == 'sp_atk':
+					p.stats['SPA'] = value
+				if key == 'sp_def':
+					p.stats['SPD'] = value
+				if key == 'speed':
+					p.stats['SPE'] = value
+				if key == 'types':
+					types = []
+					for t in value:
+						for k, v in t.items():
+							if k == 'name':
+								types.append(v)
+					p.type = types
+				if key == 'egg_groups':
+					egg_groups = []
+					for e in value:
+						for k, v in e.items():
+							if k == 'name':
+								egg_groups.append(v)
+					p.eggGroup = egg_groups
+				if key == 'moves':
+					moves = []
+					for m in value:
+						move = self.Move()
+						for k, v in m.items():
+							if k == 'name':
+								move.name = v
+							if k == 'learn_type':
+								move.learn_type = v
+						moves.append(move)
+					p.moves = moves
+				if key == 'evolutions':
+					evolution = []
+					for e in value:
+						evo = self.Evolution()
+						for k, v in e.items():
+							if k == 'method':
+								evo.method = v
+							if k == 'to':
+								evo.to = v
+						evolution.append(evo)
+					p.evolution = evolution
+
+			x.append(p)
+		self.pokemon = x
