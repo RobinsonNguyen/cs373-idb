e006a13815b1a6bffb2d3e1b1f6c45f5a9b85614
Tucker Long <tl7877@cs.utexas.edu>
2015-10-21 23:55:16 -0500

    Fixing caps in json file

--- main.py
+++ main.py
@@ -62,10 +62,8 @@ def move_id():
 @app.route('/location', methods=['GET'])
 def location_id():
     print("Testing output")
-    test = request.args.get('region')
-    print("What is region? ")
-    #//print(test)
-    #print(request.args.get("name"))
+    print(request.args.get('region'))
+    print(request.args.get("name"))
     route = routeContainer.getRouteByRegion(request.args.get("region"), request.args.get("name"))
     return render_template("routeKanto2.html", route=route)
     #return render_template('index.html')
