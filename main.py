130f82d602f4082dd90ed5eda9af24ab713dbba0
Tucker Long <tl7877@cs.utexas.edu>
2015-10-21 23:32:06 -0500

    Outputing multi params in flask

--- main.py
+++ main.py
@@ -62,6 +62,8 @@ def move_id():
 @app.route('/location', methods=['GET'])
 def location_id():
     print("Testing output")
+    print(request.args.get("region"))
+    print(request.args.get("name"))
     route = routeContainer.getRoutesByRegion(request.args.get("region"), request.args.get("name"))
     return render_template("routeKanto2.html", route=route)
     #return render_template('index.html')
