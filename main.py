84a6e555892de4b0eb9b798bb38dadc14ff85b95
Brett Kercher <brettkercher@utexas.edu>
2015-11-05 14:36:44 -0600

    fix for about page unit tests

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_test_results():
         errs, out = script.communicate()
     except:
         script.kill()
-    return jsonify({'results': out.decode()})
+    return jsonify({'results': {'out':out.decode(), 'err':errs.decode()} })
 	
 # --------
 # location
