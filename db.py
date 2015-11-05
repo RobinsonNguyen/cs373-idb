13cedae9f9fbf38e33eb9ab2ad08bb46d7213d29
Brett Kercher <brettkercher@utexas.edu>
2015-11-05 16:30:55 -0600

    removed move / location data from pokemon json parsing

--- db.py
+++ db.py
@@ -47,55 +47,7 @@ def create_pokemon():
                         if k == 'name':
                             egg_groups.append(v)
                 p.eggGroup = egg_groups
-            if key == 'moves':
-                moves = []
-                for m in value:
-                    move = self.Move()
-                    for k, v in m.items():
-                        if k == 'name':
-                            move.name = v
-                        if k == 'learn_type':
-                            move.learn_type = v
-                        if k == 'level':
-                            move.level = v
-                    moves.append(move)
-                p.moves = moves
-            if key == 'locations':
-                locations = []
-                for l in value:
-                    location = self.Location()
-                    for k, v in l.items():
-                        if k == 'game':
-                            location.game = v
-                        if k == 'method':
-                            methods = v.split(',')
-                            routes = []
-                            others = []
-                            for m in methods:
-                                if "Route" in m:
-                                    routes.append(m)
-                                else:
-                                    others.append(m)
-                            location.other = others
-                            location.routes = routes
-                    locations.append(location)
-                p.locations = locations
-            if key == 'evolutions':
-                evolution = []
-                for e in value:
-                    evo = self.Evolution()
-                    for k, v in e.items():
-                        if k == 'method':
-                            if v == 'level_up':
-                                evo.method = 'Level Up'
-                            if v == 'trade':
-                                evo.method = 'Trade'
-                            if v == 'stone':
-                                evo.method = 'Stone'
-                        if k == 'to':
-                            evo.to = v
-                    evolution.append(evo)
-                p.evolution = evolution
+                
         pokemon = Pokemon(p.name, p.stats['HP'], p.stats['ATK'], p.stats['DEF'], p.stats['SPA'], p.stats['SPD'], p.stats['SPE'])
         db.session.add(character)
         db.session.commit()
