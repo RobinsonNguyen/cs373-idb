081f1aa2589967739338adab2e8cd152ff8ba54c
sirtuck32 <t.long32@hotmail.com>
2015-11-05 03:27:46 -0600

    Pushing up sql stuff

--- main.py
+++ main.py
@@ -1,118 +1,24 @@
 from flask import Flask, render_template, abort, request, jsonify
-from models import PokemonContainer, Moves, RouteContainer
-from flask.ext.sqlalchemy import SQLAlchemy
+from models import *
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pokemon@localhost/pokemasters'
-db = SQLAlchemy(app)
-
-pokemonContainer = PokemonContainer()
-moveContainer = Moves()
-routeContainer = RouteContainer()
-
+# -----
+# index
+# -----
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#=====================Pokemon==========================================#
-
-@app.route('/pokemon/')
-def pokemon_id():
-    pokemon = pokemonContainer.GetAllPokemon()
-    return render_template('pokemon.html', pokemon=pokemon)
-
-@app.route('/pokemon/<name>/')
-def pokemon(name):
-    pokemon = pokemonContainer.GetPokemonByName(name)
-    if pokemon is None:
-        abort(404)
-    return render_template('pokemon_details.html', pokemon=pokemon)
-
-#=============API==========#
-@app.route('/api/v1.0/pokemon/', methods=['GET'])
-def get_pokemon():
-    return jsonify({'pokemon': 'test'})
-
-@app.route('/api/v1.0/pokemon/<int:id>/', methods=['GET'])
-def get_pokemon_id(id):
-    return jsonify({'pokemon': 'test'})
-
-@app.route('/api/v1.0/pokemon', methods=['POST'])
-def create_pokemon():
-    if not request.json:
-        abort(400)
-
-    return jsonify({'pokemon': 'test'}), 201
-
-@app.route('/api/v1.0/pokemon/<int:id>', methods=['PUT'])
-def update_pokemon(id):
-    if not request.json:
-        abort(400)
-    return jsonify({'pokemon': 'test'})
-
-@app.route('/api/v1.0/pokemon/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    return jsonify({'result': True})
-
-#======================================================================#
-
-@app.route('/about/')
+# --------
+# about
+# --------	
+@app.route('/about')
 def about():
     return render_template('about.html')
-
-#======================================================================#
-
-@app.route('/moves/<name>/')
-def move(name):
-    move = moveContainer.getMoveByName(name)
-    if move is None:
-        abort(404)
-    print(move.pp)
-    return render_template('moves.html', move=move)
-
-@app.route('/moves/')
-def move_id():
-	moves = moveContainer.getAllMoves()
-	return render_template('allMoves.html', moves=moves)
-
-#=============API==========#
-@app.route('/api/v1.0/moves/', methods=['GET'])
-def get_moves():
-    return jsonify({'moves': 'test'})
-
-@app.route('/api/v1.0/moves/<int:id>/', methods=['GET'])
-def get_moves_id(id):
-    return jsonify({'moves': 'test'})
-
-@app.route('/api/v1.0/moves/', methods=['POST'])
-def create_moves():
-    if not request.json:
-        abort(400)
-
-    return jsonify({'moves': 'test'}), 201
-
-@app.route('/api/v1.0/moves/<int:id>/', methods=['PUT'])
-def update_moves(id):
-    if not request.json:
-        abort(400)
-    return jsonify({'moves': 'test'})
-
-@app.route('/api/v1.0/moves/<int:id>/', methods=['DELETE'])
-def delete_moves(id):
-    return jsonify({'result': True})
-
-#======================================================================#
-
-@app.route('/location/')
-@app.route('/location', methods=['GET'])
-def location_id():
-    if request.args.get("region") is not None and request.args.get("name") is not None:
-        route = routeContainer.getRouteByRegion(request.args.get("region"), request.args.get("name"))
-        if route is None:
-            abort(404)
-        return render_template("route_data.html", route=route)
-    return render_template('location.html')
-
+	
+# --------
+# location
+# --------	
+'''
 #=============API==========#
 @app.route('/api/v1.0/locations/', methods=['GET'])
 def get_locations():
@@ -138,7 +44,84 @@ def update_locations(id):
 @app.route('/api/v1.0/locations/<int:id>/', methods=['DELETE'])
 def delete_locations(id):
     return jsonify({'result': True})
+'''
+	
+# -------
+# pokemon
+# -------
+@app.route('/pokemon/')
+@app.route('/pokemon/<name>/')
+def pokemon(name=None):
+    if name is not None:
+		return render_template('pokemon_details.html', pokemon=Pokemon.get(name))
+    return render_template('pokemon.html', pokemon=Pokemon.get_all())
+	
+#=============API==========#
+'''
+@app.route('/api/v1.0/pokemon/', methods=['GET'])
+def get_pokemon():
+    return jsonify({'pokemon': 'test'})
 
+@app.route('/api/v1.0/pokemon/<int:id>/', methods=['GET'])
+def get_pokemon_id(id):
+    return jsonify({'pokemon': 'test'})
+
+@app.route('/api/v1.0/pokemon', methods=['POST'])
+def create_pokemon():
+    if not request.json:
+        abort(400)
+
+    return jsonify({'pokemon': 'test'}), 201
+
+@app.route('/api/v1.0/pokemon/<int:id>', methods=['PUT'])
+def update_pokemon(id):
+    if not request.json:
+        abort(400)
+    return jsonify({'pokemon': 'test'})
+
+@app.route('/api/v1.0/pokemon/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    return jsonify({'result': True})
+'''
+
+# -----
+# moves
+# -----
+@app.route('/moves')
+@app.route('/moves/<name>')
+def moves(name=None):
+	if name is not None:
+		return render_template('moves.html', move=Move.get(name))
+	# change name of the above html to move and the below one to moves
+	return render_template('allMoves.html', moves=Move.get_all())
+
+#=============API==========#
+'''
+#@app.route('/api/v1.0/moves/', methods=['GET'])
+def get_moves():
+    return jsonify({'moves': 'test'})
+
+@app.route('/api/v1.0/moves/<int:id>/', methods=['GET'])
+def get_moves_id(id):
+    return jsonify({'moves': 'test'})
+
+@app.route('/api/v1.0/moves/', methods=['POST'])
+def create_moves():
+    if not request.json:
+        abort(400)
+
+    return jsonify({'moves': 'test'}), 201
+
+@app.route('/api/v1.0/moves/<int:id>/', methods=['PUT'])
+def update_moves(id):
+    if not request.json:
+        abort(400)
+    return jsonify({'moves': 'test'})
+
+@app.route('/api/v1.0/moves/<int:id>/', methods=['DELETE'])
+def delete_moves(id):
+    return jsonify({'result': True})
+'''
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
--- models.py
+++ models.py
@@ -1,402 +1,114 @@
 import json
-from SQLAlchemy import *
-from main import db
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-metadata = BoundMetaData(db)
-all_moves = Table('ALL_MOVES', metadata, autoload=True)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pokemon@localhost/pokemasters'
+db = SQLAlchemy(app)
 
-class PokemonContainer:
+class Move(db.Model):
+	__tablename__ = 'ALL_MOVES'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.VARCHAR(50))
+	type = db.Column(db.VARCHAR(50))
+	category = db.Column(db.VARCHAR(50))
+	power = db.Column(db.Integer)
+	accuracy = db.Column(db.Integer)
+	pp = db.Column(db.Integer)
+	description = db.Column(db.BLOB)
+	
+	def __init__(self, name, type, category, power, accuracy, pp, description):
+		self.name = name
+		self.type = type
+		self.category = category
+		self.power = power
+		self.accuracy = accuracy
+		self.pp = pp
+		self.description = description
+		
+	def __repr__(self):
+		"""
+        Return representation of this move in format
+        <name {}> where {} is move's name
+        """
+		return '<name {}>'.format(self.name)
+		
+	@property
+	def serialize(self):
+		return {
+            'name' : self.name,
+            'type' : self.type,
+            'category' : self.category,
+            'power' : self.power,
+            'accuracy' : self.accuracy,
+            'pp' : self.pp,
+            'description' : self.description}
+		
+	@staticmethod
+	def get_all():
+		return Move.query.all()
+		
+	@staticmethod
+	def get(move):
+		return Move.query.filter_by(name=move).first()
+		
 
-	class Move:
-		def __init__(self):
-			self.name = ""
-			self.learn_type = ""
-			self.level = ""
-			self.tm = ""
-
-	class Evolution:
-		def __init__(self):
-			self.to = ""
-			self.method = ""
-			self.level = 0
-			
-	class Location:
-		def __init__(self):
-			self.other = []
-			self.routes = []
-			self.game = ""
-
-	class Pokemon:
-		def __init__ (self):
-			self.id = 0
-			self.name = ""
-			self.type = ()
-			self.eggGroup = ()
-			self.stats = {}
-			self.imgPath = ""
-			self.moves = []
-			self.evolution = []
-
-	def __init__ (self):
-		self.ReadPokemonData()
-
-	def GetPokemonById(self, id):
-		for p in self.pokemon:
-			if p.id is id:
-				return p
-	def GetPokemonByName(self, name):
-		for p in self.pokemon:
-			if p.name == name:
-				return p
-
-	def GetAllPokemon(self):
-		return self.pokemon
-
-	def ReadPokemonData(self):
-		x = []
-
-		path = "./static/json/pokemon_data_Version2.json"
-		inputFile = open(path)
-		data = json.loads(inputFile.read())
-		inputFile.close()
-		pokeList = data['pokemon']
-
-		for poke in pokeList:
-			p = self.Pokemon()
-			for key, value in poke.items():
-				if key == 'pkdx_id':
-					p.id = value
-				if key == 'name':
-					p.name = value
-					p.imgPath = value + '_regular_.png'
-				if key == 'hp':
-					p.stats['HP'] = value
-				if key == 'attack':
-					p.stats['ATK'] = value
-				if key == 'defense':
-					p.stats['DEF'] = value
-				if key == 'sp_atk':
-					p.stats['SPA'] = value
-				if key == 'sp_def':
-					p.stats['SPD'] = value
-				if key == 'speed':
-					p.stats['SPE'] = value
-				if key == 'types':
-					types = []
-					for t in value:
-						for k, v in t.items():
-							if k == 'name':
-								types.append(v)
-					p.type = types
-				if key == 'egg_groups':
-					egg_groups = []
-					for e in value:
-						for k, v in e.items():
-							if k == 'name':
-								egg_groups.append(v)
-					p.eggGroup = egg_groups
-				if key == 'moves':
-					moves = []
-					for m in value:
-						move = self.Move()
-						for k, v in m.items():
-							if k == 'name':
-								move.name = v
-							if k == 'learn_type':
-								move.learn_type = v
-							if k == 'level':
-								move.level = v
-						moves.append(move)
-					p.moves = moves
-				if key == 'locations':
-					locations = []
-					for l in value:
-						location = self.Location()
-						for k, v in l.items():
-							if k == 'game':
-								location.game = v
-							if k == 'method':
-								methods = v.split(',')
-								routes = []
-								others = []
-								for m in methods:
-									if "Route" in m:
-										routes.append(m)
-									else:
-										others.append(m)
-								location.other = others
-								location.routes = routes
-						locations.append(location)
-					p.locations = locations
-				if key == 'evolutions':
-					evolution = []
-					for e in value:
-						evo = self.Evolution()
-						for k, v in e.items():
-							if k == 'method':
-								if v == 'level_up':
-									evo.method = 'Level Up'
-								if v == 'trade':
-									evo.method = 'Trade'
-								if v == 'stone':
-									evo.method = 'Stone'
-							if k == 'to':
-								evo.to = v
-						evolution.append(evo)
-					p.evolution = evolution
-
-			x.append(p)
-		self.pokemon = x
-
-class Moves(db.ALL_MOVES):
-
-
-
-	class pokemon:
-
-		def __init__(self):
-			self.id = 0
-			self.name = ""
-			self.lvl = 0
-			self.tm = ""
-			self.hp = 0
-			self.atk = 0
-			self.defs = 0
-			self.speed = 0
-			self.sp_atk = 0
-			self.sp_def = 0
-			self.type = ()
-
-
-	class move:
-
-		def __init__(self):
-			self.id = 0
-			self.name = ""
-			self.tm = ""
-			self.type = ""
-			self.category = ""
-			self.power = ""
-			self.accuracy = ""
-			self.pp = ""
-			self.description = ""
-			self.pokemonLVL= []
-			self.pokemonTM= []
-
-	def __init__(self):
-		self.ReadMoves()
-		self.ReadAllMoves()
-
-	def ReadMoves(self):
-		pokemonContainer = PokemonContainer()
-		p = pokemonContainer.GetAllPokemon()
-
-		path = "./static/json/moves_final.json"
-		inputFile = open(path)
-		data = json.loads(inputFile.read())
-		inputFile.close()
-
-		moves = []
-
-		for x in data['moves']:
-			m = self.move()
-			for key, value in x.items():
-				if key == "id":
-					m.id = value
-				elif key == 'name':
-					m.name = value
-				elif key == 'tm':
-					m.tm = value
-				elif key == 'type':
-					m.type = value
-				elif key == 'category':
-					m.category = value
-				elif key == 'power':
-					m.power = value
-				elif key == 'accuracy':
-					m.accuracy = value
-				elif key == 'pp':
-					m.pp = value
-				elif key == 'description':
-					m.description = value
-
-			for x in p:
-				poke = self.pokemon()
-				for y in x.moves:
-					if y.name == m.name:
-						if y.learn_type == "level up":
-							poke.id = x.id
-							poke.name = x.name
-							poke.lvl = y.level
-							poke.hp = x.stats["HP"]
-							poke.atk = x.stats["ATK"]
-							poke.defs = x.stats["DEF"]
-							poke.speed = x.stats["SPE"]
-							poke.sp_atk = x.stats["SPA"]
-							poke.sp_def = x.stats["SPD"]
-							poke.type = x.type
-							poke.img = x.imgPath
-							m.pokemonLVL.append(poke)
-						elif y.learn_type == "machine":
-							poke.id = x.id
-							poke.name = x.name
-							poke.tm = m.tm
-							poke.hp = x.stats["HP"]
-							poke.atk = x.stats["ATK"]
-							poke.defs = x.stats["DEF"]
-							poke.speed = x.stats["SPE"]
-							poke.sp_atk = x.stats["SPA"]
-							poke.sp_def = x.stats["SPD"]
-							poke.type = x.type
-							poke.img = x.imgPath
-							m.pokemonTM.append(poke)
-
-			moves.append(m)
-
-		self.moves = moves
-
-	def ReadAllMoves(self):
-
-		path = "./static/json/moves_final.json"
-		inputFile = open(path)
-		data = json.loads(inputFile.read())
-		inputFile.close()
-
-		moves = []
-
-		for x in data['moves']:
-			m = self.move()
-			for key, value in x.items():
-				if key == "id":
-					m.id = value
-				elif key == 'name':
-					m.name = value
-				elif key == 'power':
-					m.power = value
-				elif key == 'accuracy':
-					m.accuracy = value
-				elif key == 'pp':
-					m.pp = value
-			moves.append(m)
-
-		self.Allmoves = moves
-
-	def getAllMoves(self):
-		return db.execute('select * from ALL_MOVES', [1]).first()
-
-	def getMoveByName(self, name):
-		return all_moves.select(all_moves.c.MOVE_NAME == name).execute().first()
-
-class RouteContainer:
-
-	class item:
-		def __init__(self):
-			self.name = ""
-			self.area = ""
-			self.description = ""
-
-	class trainer:
-		def __init__(self):
-			self.name = ""
-			self.area = ""
-			self.pokemon = []
-
-	class trainerPokemon:
-		def __init__(self):
-			self.name = ""
-			self.img = ""
-			self.level = ""
-
-	class wildPokemon:
-		def __init__(self):
-			self.name = ""
-			self.img = ""
-			self.area = ""
-			self.levels = ""
-			self.rate = ""
-
-	class route:
-		def __init__(self):
-			self.region = ""
-			self.name = ""
-			self.prevRoute = ""
-			self.nextRoute = ""
-			self.routeImg = ""
-			self.routeDesc = ""
-			self.pokemon = []
-			self.trainers = []
-			self.items = []
-
-	def __init__(self):
-		self.readInRoutes()
-
-	def readInRoutes(self):
-		inputFile = open("./static/json/routes.json")
-		routeData = json.loads(inputFile.read())
-		inputFile.close()
-		routes = []
-
-		for r in routeData["routes"]:
-			newRoute = self.route()
-			for key, value in r.items():
-				if key == "region":
-					newRoute.region = value
-				if key == "name":
-					newRoute.name = value
-				if key == "prevRoute":
-					newRoute.prevRoute = value
-				if key == "nextRoute":
-					newRoute.nextRoute = value
-				if key == "routeImg":
-					newRoute.routeImg = value
-				if key == "routeDesc":
-					newRoute.routeDesc = value
-				if key == "wildPokemon":
-					for val in value:
-						newPokemon = self.wildPokemon()
-						if 'name' in val:
-							newPokemon.name = str(val['name'])
-						if 'img' in val:
-							newPokemon.img = val['img']
-						if 'area' in val:
-							newPokemon.area = val['area']
-						if 'levels' in val:
-							newPokemon.levels = val['levels']
-						if 'rate' in val:
-							newPokemon.rate = val['rate']
-						newRoute.pokemon.append(newPokemon)
-				if key == "trainers":
-					for val in value:
-						newTrainer = self.trainer()
-						if 'name' in val:
-							newTrainer.name = val['name']
-						if 'area' in val:
-							newTrainer.area = val['area']
-						if 'pokemon' in val:
-							for v in val["pokemon"]:
-								newTrainerPokemon = self.trainerPokemon()
-								if "name" in v:
-									newTrainerPokemon.name = v["name"]
-								if "img" in v:
-									newTrainerPokemon.img = v["img"]
-								if "level" in v:
-									newTrainerPokemon.level = v["level"]
-								newTrainer.pokemon.append(newTrainerPokemon)
-						newRoute.trainers.append(newTrainer)
-				if key == "items":
-					for val in value:
-						newItem = self.item()
-						if "name" in val:
-							newItem.name = val["name"]
-						if "area" in val:
-							newItem.area = val["area"]
-						if "description" in val:
-							newItem.description = val["description"]
-						newRoute.items.append(newItem)
-			routes.append(newRoute)
-		self.routes = routes;
-
-	def getRouteByRegion(self, region, name):
-		for r in self.routes:
-			if r.region == region and r.name == name:
-				return r
+	
+class Pokemon(db.Model):
+	__tablename__ = "pokemon"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.VARCHAR(50))
+	hp = db.Column(db.Integer)
+	attack = db.Column(db.Integer)
+	defense = db.Column(db.Integer)
+	spAttack = db.Column(db.Integer)
+	spDefense = db.Column(db.Integer)
+	speed = db.Column(db.Integer)
+	stats = []
+	
+	def __init__(self, name, hp, attack, defense, spAttack, spDefense, speed):
+		self.name = name
+		self.hp = hp
+		self.attack = attack
+		self.defense = defense
+		self.spAttack = spAttack
+		self.spDefense = spDefense
+		self.speed = speed
+		
+	def __repr__(self):
+		return '<name {}>'.format(self.name)
+	
+	@property
+	def serialize(self):
+		dtest = {
+			'name' : self.name,
+			'hp' : self.hp, 
+			'atk' : self.attack, 
+			'def' : self.defense, 
+			'spa' : self.spAttack, 
+			'spd' : self.spDefense, 
+			'spe' : self.speed,
+			'moves' : test()}
+		print(dtest)
+		return dtest
+	
+	def test(self):
+		return {{'name' : 'Tackle', 'learn_type' : 'level' }}
+	
+	@staticmethod
+	def get_all():
+		return Pokemon.query.all()
+		
+	@staticmethod
+	def get(name):
+		return Pokemon.query.filter_by(name=name).first()
+		
+class Pokemon_Moves(db.Model):
+	__tablename__ = "pokemon_moves"
+	id = db.Column(db.Integer, primary_key=True)
+	pokemon_id = db.Column(db.Integer)
+	move_id = db.Column(db.Integer)
+	
+	def __init__(self, pokemon_id, move_id):
+		self.pokemon_id = pokemon_id
+		self.move_id = move_id
