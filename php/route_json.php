54b63f8c763497816703308c63650b449f388019
Tucker Long <tl7877@cs.utexas.edu>
2015-11-16 14:51:35 -0600

    Trying to change blobs to text for mysql server

--- php/route_json.php
+++ php/route_json.php
@@ -4,14 +4,14 @@
 	$con->set_charset('utf8');
 	$drop = mysqli_prepare($con, 'DROP TABLE IF EXISTS ALL_ROUTES;') or die(mysqli_error($con));
 
-	$create = mysqli_prepare($con, 'CREATE TABLE ALL_ROUTES(ID INT NOT NULL AUTO_INCREMENT, ROUTE_NAME VARCHAR(50) NOT NULL, ROUTE_REGION VARCHAR(50) NOT NULL, ROUTE_NORTH_EXIT VARCHAR(50), ROUTE_SOUTH_EXIT VARCHAR(50), ROUTE_EAST_EXIT VARCHAR(50), ROUTE_WEST_EXIT VARCHAR(50), ROUTE_ACCESS_TO VARCHAR(50), ROUTE_MINI_DESCRIPTION VARCHAR(256), ROUTE_MAIN_DESCRIPTION BLOB, ROUTE_TRIVIA BLOB, PRIMARY KEY(ID));') or die(mysqli_error($con));
+	$create = mysqli_prepare($con, 'CREATE TABLE ALL_ROUTES(ID INT NOT NULL AUTO_INCREMENT, ROUTE_NAME VARCHAR(50) NOT NULL, ROUTE_REGION VARCHAR(50) NOT NULL, ROUTE_NORTH_EXIT VARCHAR(50), ROUTE_SOUTH_EXIT VARCHAR(50), ROUTE_EAST_EXIT VARCHAR(50), ROUTE_WEST_EXIT VARCHAR(50), ROUTE_ACCESS_TO VARCHAR(50), ROUTE_MINI_DESCRIPTION VARCHAR(256), ROUTE_MAIN_DESCRIPTION TEXT, ROUTE_TRIVIA TEXT, PRIMARY KEY(ID));') or die(mysqli_error($con));
 
     mysqli_stmt_execute($drop) or die(mysqli_error($con));
     mysqli_stmt_execute($create) or die(mysqli_error($con));
 
     $st = mysqli_prepare($con, 'INSERT INTO ALL_ROUTES(ROUTE_NAME, ROUTE_REGION, ROUTE_NORTH_EXIT, ROUTE_SOUTH_EXIT, ROUTE_EAST_EXIT, ROUTE_WEST_EXIT, ROUTE_ACCESS_TO, ROUTE_MINI_DESCRIPTION, ROUTE_MAIN_DESCRIPTION, ROUTE_TRIVIA) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)') or die(mysqli_error($con));
 
-    mysqli_stmt_bind_param($st, 'ssssssssbb', $route_name, $region_name, $nexit, $sexit, $eexit, $wexit, $accessto, $mindesc, $maindesc, $trivia);
+    mysqli_stmt_bind_param($st, 'ssssssssss', $route_name, $region_name, $nexit, $sexit, $eexit, $wexit, $accessto, $mindesc, $maindesc, $trivia);
 
     //read the json file contents
     $jsondata = file_get_contents('/home/www/cs373-idb/SomeScrapingData/FINAL_ROUTE_INFO.json');
