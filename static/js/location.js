8ed3d02e5683c25af101a713f5b79e19a2b7b631
Tucker Long <tl7877@cs.utexas.edu>
2015-11-16 14:19:56 -0600

    Adding in all gen buttons

--- static/js/location.js
+++ static/js/location.js
@@ -6,22 +6,75 @@ $(document).ready(function(){
 
 }); 
 
-function genI() {
-	var genI = document.getElementsByClassName("genIPokemon");
-	for(i = 0; i < genI.length; i++)
-		genI[i].style.display = "table-row";
-
-	var genII = document.getElementsByClassName("genIIPokemon");
-	for(i = 0; i < genII.length; i++)
-		genII[i].style.display = "none";
+function lowestGen() {
+	clearGen();
 }
 
-function genII() {
+function clearGen() {
 	var genI = document.getElementsByClassName("genIPokemon");
 	for(i = 0; i < genI.length; i++)
 		genI[i].style.display = "none";
 
 	var genII = document.getElementsByClassName("genIIPokemon");
 	for(i = 0; i < genII.length; i++)
+		genII[i].style.display = "none";
+
+	var genIII = document.getElementsByClassName("genIIIPokemon");
+	for(i = 0; i < genIII.length; i++)
+		genIII[i].style.display = "none";
+
+	var genIV = document.getElementsByClassName("genIVPokemon");
+	for(i = 0; i < genIV.length; i++)
+		genIV[i].style.display = "none";
+
+	var genV = document.getElementsByClassName("genVPokemon");
+	for(i = 0; i < genV.length; i++)
+		genV[i].style.display = "none";
+
+	var genVI = document.getElementsByClassName("genVIPokemon");
+	for(i = 0; i < genVI.length; i++)
+		genVI[i].style.display = "none";
+
+}
+
+function genI() {
+	clearGen();
+	var genI = document.getElementsByClassName("genIPokemon");
+	for(i = 0; i < genI.length; i++)
+		genI[i].style.display = "table-row";
+}
+
+function genII() {
+	clearGen();
+	var genII = document.getElementsByClassName("genIIPokemon");
+	for(i = 0; i < genII.length; i++)
 		genII[i].style.display = "table-row";
+}
+
+function genIII() {
+	clearGen();
+	var genIII = document.getElementsByClassName("genIIIPokemon");
+	for(i = 0; i < genIII.length; i++)
+		genIII[i].style.display = "table-row";
+}
+
+function genIV() {
+	clearGen();
+	var genIV = document.getElementsByClassName("genIVPokemon");
+	for(i = 0; i < genIV.length; i++)
+		genIV[i].style.display = "table-row";
+}
+
+function genV() {
+	clearGen();
+	var genIV = document.getElementsByClassName("genVPokemon");
+	for(i = 0; i < genV.length; i++)
+		genV[i].style.display = "table-row";
+}
+
+function genIV() {
+	clearGen();
+	var genVI = document.getElementsByClassName("genVIPokemon");
+	for(i = 0; i < genVI.length; i++)
+		genIV[i].style.display = "table-row";
 }
