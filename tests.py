5fc9332daca2ee21371304654cfc34959dfa4c48
Robinson Nguyen <robinson_nguyen@hotmail.com>
2015-11-16 18:17:47 -0600

    Adding unit tests

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from unittest import main, TestCase
 from models import Pokemon, Move, Routes
-from models import db
-from flask import Flask
+# from models import db
+# from flask import Flask
 
 
 class UnitTestModels(TestCase):
@@ -129,14 +129,14 @@ class UnitTestModels(TestCase):
 		# self.assertEqual(allRoutes.routes[2].trainers[0].name, "Bug Catcher Wade")
 		# self.assertEqual(allRoutes.routes[2].items[0].name, "Rest")
 
-	def test_add_pokemon_1(self):
-		poketest = Pokemon(POKEMON_ID = 0, POKEMON_NAME = "Lazy Fox", POKEMON_HEIGHT = 2)
-		db.session.add(poketest)
-		db.session.commit()
-		test = Pokemon.query.filter_by(POKEMON_ID=0).first()
-		self.assertEqual(test.POKEMON_NAME, "Lazy Fox")
-		self.assertEqual(True,False)
-		db.session.delete(test)
+	# def test_add_pokemon_1(self):
+	# 	poketest = Pokemon(POKEMON_ID = 0, POKEMON_NAME = "Lazy Fox", POKEMON_HEIGHT = 2)
+	# 	db.session.add(poketest)
+	# 	db.session.commit()
+	# 	test = Pokemon.query.filter_by(POKEMON_ID=0).first()
+	# 	self.assertEqual(test.POKEMON_NAME, "Lazy Fox")
+	# 	self.assertEqual(True,False)
+	# 	db.session.delete(test)
 
 if __name__ == "__main__" : 
 	main()
