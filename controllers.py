0df2a912fa2dde683681887272a1bd1882cb841d
Tucker Long <tl7877@cs.utexas.edu>
2015-11-17 17:21:17 -0600

    Removing urllib for now since it doesn't run

--- controllers.py
+++ controllers.py
@@ -2,11 +2,11 @@ import json
 import urllib
 import subprocess
 
-from urllib2.request import Request, urlopen, URLError
+#from urllib2.request import Request, urlopen, URLError
 
 def politican_controller():
-	politician_url = urllib.request.urlopen("http://politicianhub.me/api/v1/legislators")
-	politicians = json.load(politician_url.read().decode('utf-8'))
+	#politician_url = urllib.request.urlopen("http://politicianhub.me/api/v1/legislators")
+	#politicians = json.load(politician_url.read().decode('utf-8'))
 
 	democratic_politicians = {}
 
