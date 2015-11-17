80ac1fb0d1f598bffa71053cd15aac6f60b64b25
Tucker Long <tl7877@cs.utexas.edu>
2015-11-17 17:10:38 -0600

    Adding politician page

---
+++ controllers.py
@@ -0,0 +1,13 @@
+import json
+import urllib
+import subprocess
+
+from urllib.request import Request, urlopen, URLError
+
+def politican_controller():
+	politician_url = urllib.request.urlopen("http://politicianhub.me/api/v1/legislators")
+	politicians = json.load(politician_url.read().decode('utf-8'))
+
+	democratic_politicians = {}
+
+	return democratic_politicians 
