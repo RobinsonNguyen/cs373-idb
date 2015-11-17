f8d026df9eb614c0d6eb78c71477f089558a643e
Tucker Long <tl7877@cs.utexas.edu>
2015-11-17 17:17:46 -0600

    chainging to urllib2

--- controllers.py
+++ controllers.py
@@ -2,7 +2,7 @@ import json
 import urllib
 import subprocess
 
-from urllib.request import Request, urlopen, URLError
+from urllib2.request import Request, urlopen, URLError
 
 def politican_controller():
 	politician_url = urllib.request.urlopen("http://politicianhub.me/api/v1/legislators")
