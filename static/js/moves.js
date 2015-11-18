4b22333fcd685e301de7095b0318189a8d34bb4c
David Toth <dtoth@cs.utexas.edu>
2015-11-18 17:17:06 -0600

    Collapsing moves

--- static/js/moves.js
+++ static/js/moves.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
 		location.href = '/moves/' + e[0].trim();
 	});
 
-	$('.stats').click(function(e){
-	    $(e).slideToggle('slow');
+	$('.stats').click(function(caller){
+	    $(this).children('p').slideToggle('slow');
 	});
 
 }); 
+
+function collapse(caller) {
+	console.log("test");
+	caller.style.height = "500px";
+}
