1a8d1c4d29acf21c7b65e5821c2fb04dd4abbe64
Brett Kercher <brettkercher@utexas.edu>
2015-11-18 16:57:24 -0600

    toggle

--- static/js/moves.js
+++ static/js/moves.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
 		location.href = '/moves/' + e[0].trim();
 	});
 
-	$('.stats').click(function(){
-		console.log("tt");
-	    $('.data', this).toggle();
+	$('.stats').click(function(e){
+	    $(e.target).find('.data').slideToggle('slow');
 	});
 
 }); 
