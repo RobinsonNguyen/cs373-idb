cc43aad06c6ed121faf63bed9cf8c3b8fa36ee56
Tucker Long <tl7877@cs.utexas.edu>
2015-11-17 19:02:52 -0600

    Fixed missing trainers and attempting to make tables smaller

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_test_results():
 @app.route('/location/<name>')
 def location_id(name=None):
     if name is not None:
-        return render_template("route_data.html", route=Routes.get(name), pokemon=RoutePokemon.get(name), images=RouteImages.get(name))
+        return render_template("route_data.html", route=Routes.get(name), pokemon=RoutePokemon.get(name), images=RouteImages.get(name), trainers=RouteTrainers.get(name))
     return render_template('location.html', routes=Routes.get_all())
 
 #=============API==========#
