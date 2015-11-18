9f9efb7e9daa6fd236ab750ad693a22698f4ed2f
Tucker Long <tl7877@cs.utexas.edu>
2015-11-17 18:26:24 -0600

    Adding politician page back in

--- controllers.py
+++ controllers.py
@@ -1,13 +1,31 @@
 import json
-import urllib
+import urllib2
 import subprocess
 
-#from urllib2.request import Request, urlopen, URLError
-
-def politican_controller():
+def politician_controller():
 	#politician_url = urllib.request.urlopen("http://politicianhub.me/api/v1/legislators")
 	#politicians = json.load(politician_url.read().decode('utf-8'))
 
-	democratic_politicians = {}
+	req = urllib2.Request("http://politicianhub.me/api/v1/legislators")
+	url = urllib2.urlopen(req)
+	data = json.load(url)
+	politicians = data['legislators']
+
+	democratic_politicians = []
+
+	for t in test:
+		poli = {}
+		for k, v in t.items():
+			if(k == 'first_name')
+				poli['firstName'] = v
+			if(k == 'last_name')
+				poli['last_name'] = v
+			if(k == 'twitter')
+				poli['twitter'] = v
+			if(k == 'state')
+				poli['state'] = v
+			if(k == 'birthday')
+				poli['birthday'] = v
+		democratic_politicians.append(poli)
 
 	return democratic_politicians 
