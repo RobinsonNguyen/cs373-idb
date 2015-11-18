a6fff5203f79c0ee2dfb55fee1d141ac6d9e83fd
David Toth <dtoth@cs.utexas.edu>
2015-11-18 14:26:13 -0600

    trying to fix move level learned open issue #42

--- php/pokemon_moves_json.php
+++ php/pokemon_moves_json.php
@@ -24,6 +24,7 @@
     	foreach($row['moves'] as $r){
 			$mname = $r['name'];
 			$learn = $r['learn_type'];
+			$level = NULL
 			if (array_key_exists('level', $r)){
 				$level = $r['level'];
 			}
