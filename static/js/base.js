90451ef1cb32f3ca85dd196292a57a8116ea5e1d
Tucker Long <tl7877@cs.utexas.edu>
2015-11-19 16:30:16 -0600

    Adding js back in

--- static/js/base.js
+++ static/js/base.js
@@ -9,3 +9,19 @@ $(document).ready(function() {
 	});
 
 }); 
+
+$(document).ready(function(){
+	
+	$('#route-models').on('click-row.bs.table', function(row, e) {
+		location.href = '/location/' + e[0].trim();
+	});
+
+}); 
+
+$(document).ready(function(){
+	
+	$('#route-pokemon-models').on('click-row.bs.table', function(row, e) {
+		location.href = '/pokemon/' + e[1].trim();
+	});
+
+}); 
